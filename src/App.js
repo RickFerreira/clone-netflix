0199cec1e6f2d00c93ff1e84b3f290fbd62e877d
Richard Ferreira <richard.salviano@academico.ifpb.edu.br>
2022-06-19 01:06:03 -0300

    lista de filmes organizadas css base

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import Tmdb from "./Tmdb";
 import MovieRow from "./components/MovieRow";
+import './App.css';
+
 
 export default () => {
   //Pegar a lista de filmes
